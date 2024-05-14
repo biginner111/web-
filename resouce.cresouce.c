e33af64653ad63cb5c240f81c353e67e837cbea4
biginner111 <142087425+biginner111@users.noreply.github.com>
2024-05-14 19:52:15 +0800

    cresouce.c

--- resouce.cresouce.c
+++ resouce.cresouce.c
@@ -1,32 +1,32 @@
-//根据指定路径判断文件是否可读和查找资源对应类型
-#include "resource.h"
+//资源管理模块实现
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<stdio.h>
+#include<string.h>
+#include"resource.h"
+#include"mime.h"
 
-//根据指定后缀区查询资源类型
-int findtype(const char* path,char *type)
-{
- char* buf=strrchr(path,'.');
- if(buf==NULL)
- {
-  printf("%d:%ld无法获取文件扩展名\n",getpid(),syscall(SYS_gettid));
-  return -1;
- }
- //查找对应类型
- int i;
- for(i=0;i<(sizeof(s_mime)/sizeof(s_mime[1]));i++)
- {
- if(strcasecmp(buf,s_mime[i].suffix)==0)
-   {
-     strcpy(type,s_mime[i].type);
-     return 0;
-   }
- }
- printf("%d:%ld:找不到对应类型\n",getpid(),syscall(SYS_gettid));
+//搜索资源
+int searchResource(const char* path){
+    return access(path,R_OK);
 }
 
+//判断类型
+int identifyType(const char* path,char* type){
+    // /c/hello.html
+    //先找.
+    char* dot = strrchr(path,'.');
+    if(dot == NULL){
+        printf("%d.%ld > 无法获取文件拓展名\n",getpid(),syscall(SYS_gettid));
+        return -1;
+    }
 
-
-//判断当前进程对找到的文件是否可读
-int processread(const char* path)
-{
- return access(path,R_OK);
+    for(int i = 0;i < sizeof(s_mime) / sizeof(s_mime[0]);i++){
+        if(strcasecmp(dot,s_mime[i].suffix) == 0){
+            strcpy(type,s_mime[i].type);
+            return 0;
+        }
+    }
+    printf("%d.%ld > 不可识别的拓展名类型\n",getpid(),syscall(SYS_gettid));
+    return -1;
 }
